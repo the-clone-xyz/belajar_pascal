9be48ba5d8aca52cba019ca20f6989a9fdf3d657
the-clone-xyz <nurhalimahukk@gmail.com>
2024-11-11 13:07:13 +0700

    menambahkan file tugas

---
+++ kampus/pertemuan 4/tugas.pas
@@ -0,0 +1,10 @@
+program tugas;
+uses crt;
+begin
+  clrscr();
+  gotoXY(49, 1); WriteLn('NAMA  : ');
+  gotoXY(49, 2); WriteLn('NIM   : ');
+  gotoXY(49, 3); WriteLn('PRODI : TEKNIK INFORMASIKA');
+  gotoXY(49, 4); WriteLn('KELAS : TIFA 3');
+  readln;
+end.
